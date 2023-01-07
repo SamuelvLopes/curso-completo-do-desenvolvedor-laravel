d5d511cea77a5f602e06e89229b779f25e3e3c1d
Samuel Lopes <samek.vitor1@gmail.com>
2023-01-07 01:16:34 -0300

    34. Super Gestão - Criando o menu de navegação

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,31 @@
-<?=__FILE__?>
+<h3><?=__FILE__?></h3>
+
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+    <a href="regexnoparam/hijbnk/5">regex</a>
+    </li>
+    <li>
+       
+    <a href="sobre-nos">sobre nos</a>
+    </li>
+    <li>
+        <a href="contato">contato</a>
+    </li>
+    <li>
+        <a href="pao2/fgfh">ultimo eh opcional</a>
+    </li>
+    <li>
+        <a href="pao/livro1/2">ultimo n eh opcional</a>
+    </li>
+    <li>
+        <a href="contato/livro2/3">Contato livro pagfina</a>
+    </li>
+    <li>
+        <a href="raiz">RAIZ</a>
+    </li>
+</ul>    
+        
