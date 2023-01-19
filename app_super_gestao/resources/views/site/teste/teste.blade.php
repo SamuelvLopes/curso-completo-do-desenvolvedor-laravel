a1fd653ee3d7ae1dc4cc93da8940f2f445143060
Samuel Lopes <samek.vitor1@gmail.com>
2023-01-19 12:47:13 -0300

    assets helper

--- app_super_gestao/resources/views/site/teste/teste.blade.php
+++ app_super_gestao/resources/views/site/teste/teste.blade.php
@@ -1,3 +1,13 @@
 a={{$a}}
 <hr>
 b={{$b}}
+<hr>
+<img src="{{asset('/assets/imagens/1.jpg')}}">
+<hr>
+<img src="{{asset('/assets/imagens/2.jpg')}}">
+<hr>
+<img src="{{asset('/assets/imagens/3.jpg')}}">
+<hr>
+<img src="{{asset('/assets/imagens/4.jpg')}}">
+<hr>
+
