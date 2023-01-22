d0e571185d5525992d501560ffef73f8855749a2
Samuel Lopes <samek.vitor1@gmail.com>
2023-01-22 18:18:27 -0300

    falha n pegou no construtor

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Middleware\LogAcessoMiddleware;
 class ContatoController extends Controller
 {
+
+    public function __construct()
+    {
+        
+        $this->middleware(LogAcessoMiddleware::class);
+
+        
+    }
     public function contato():mixed{
 
         return view('site.contato');
